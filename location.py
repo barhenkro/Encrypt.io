c7b768c8e69e4aaa7eb427381841167db211bc6a
bar-hen <bar.hen.kro@gmail.com>
2018-06-06 00:25:11 +0300

    added get_tree funcion. the  function return the directory tree from the given path

--- location.py
+++ location.py
@@ -1,7 +1,9 @@
 import win32api
 import win32con
+import os
 
 def is_attrib_file(file_name, attrib):
+    
     file_attrib = win32api.GetFileAttributes(file_name) 
     return attrib & file_attrib == attrib
 
@@ -24,23 +26,48 @@ def is_directory(dir_name):
     return is_attrib_file(dir_name, win32con.FILE_ATTRIBUTE_DIRECTORY)
 
 def is_suitable_file(file_name):
-    if is_system_file(file_name) or is_device_file(file_name)\
-    or is_readonly_file(file_name) or is_temp_file(file_name)\
-    or is_virtual_file(file_name):
+    try:
+        if is_system_file(file_name) or is_device_file(file_name)\
+        or is_readonly_file(file_name) or is_temp_file(file_name)\
+        or is_virtual_file(file_name):
+            return False
+        return True
+    except:
         return False
-    return True
 
-def get_dirs():
+def get_disks():
     drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
+
+def get_tree(path):
+    tree=[]
+    root, dirs, files=next(os.walk(path))
+    for f in files:
+        print (os.path.join(path,f))
+        if is_suitable_file(os.path.join(path,f)):
+            tree.append(f)
+    for d in dirs:
+        print (os.path.join(path,d))
+        if is_suitable_file(os.path.join(path,d)):
+            try:
+                tree.append((d, get_tree(os.path.join(root,d))))
+            except StopIteration:
+               pass 
+    return tree
+    
+    
+    
     
 
 
 
 
-print is_system_file(r'C:\Users\student\Desktop\New Text Document.txt')
-print is_device_file(r'C:\Users\student\Desktop\New Text Document.txt')
-print is_directory(r'C:\Users\student\Desktop')
-print is_readonly_file(r'C:\Users\student\Desktop\New Text Document.txt')
-print is_suitable_file(r'C:\Users\student\Desktop\New Text Document.txt')
+#print is_system_file(r'C:\Users\shaked\Desktop\hi')
+#print is_device_file(r'C:\Users\shaked\Desktop\hi')
+#print is_directory(r'C:\Users\shaked\Desktop')
+#print is_readonly_file(r'C:\Users\shaked\Desktop\hi')
+#print is_suitable_file(r'C:\Users\shaked\Desktop\hi')
+a=get_tree('C:\\')
+#next(os.walk(r'C:\ProgramData\Avg\Antivirus\SecureLine'))
+
 
 
