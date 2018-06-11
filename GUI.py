07db0515b80f2dec913651525cd9ff67da1cafa9
bar-hen <bar.hen.kro@gmail.com>
2018-06-12 01:24:26 +0300

    started the official GUI

--- GUI.py
+++ GUI.py
@@ -1,39 +1,110 @@
-from tkinter import *
-import server
+from Tkinter import *
+import ttk
+from server import *
+import os
 
 class GUI(object):
     def __init__(self):
-        self.root = Tk()
-        self.server = server.Server()
+        self.server = Server()
         self.server.run_thread()
+        self.ip_list = []# self.server.get_ip_list()
 
-        self.enteries = Frame(self.root)
-        self.ip = Entry(self.enteries)
-        self.file = Entry(self.enteries)
+        self.files = []
+        self.folders = []
+        self.tree = []
 
-        self.buttons = Frame(self.root)
+        root = Tk()
+        up_frame = Frame(root)
+        up_frame.pack()
 
-        # enc_fun = lambda: self.server.encrypt(self.ip.get(), self.file.get())
-        # dec_fun = lambda: self.server.decrypt(self.ip.get(), self.file.get())
-        self.encrypt_button = Button(self.buttons, text='encrypt', command=self.encrypt )
-        self.decrypt_button = Button(self.buttons, text='decrypt', command=self.decrypt )
+        bottomframe = Frame(root)
+        bottomframe.pack( side = BOTTOM)
 
-        self.enteries.pack()
-        self.buttons.pack(side=BOTTOM)
+        bottom_left_frame=Frame(bottomframe)
+        bottom_left_frame.pack( side = LEFT)
 
-        self.ip.pack(side=LEFT)
-        self.file.pack(side=RIGHT)
+        self.tree_frame=Frame(bottomframe)
+        self.tree_frame.pack( side = RIGHT)
 
-        self.encrypt_button.pack(side=LEFT)
-        self.decrypt_button.pack(side=RIGHT)
+        self.ips=ttk.Combobox(up_frame, values= self.ip_list, postcommand=self.update_ips , width=80, state='readonly')
+        self.ips.pack()
+        # Thread(target=self.update_ips).start()
 
-        self.root.mainloop()
+        self.path = []
+
+        self.scrollbar = Scrollbar(self.tree_frame)
+        self.scrollbar.pack(side=LEFT, fill=Y)
+
+        self.tree_list = Listbox(self.tree_frame, yscrollcommand=self.scrollbar.set)
+        # self.update_scrollbar(['a','b','c',('sdsdsd',['as','bs']),'file'])
+
+        self.tree_list.pack(side=LEFT, fill=BOTH)
+        self.scrollbar.config(command=self.tree_list.yview)
+
+        self.tree_frame.pack()
+
+
+
+        back_botton=Button(bottom_left_frame,text="<| Back",fg="Black", width=7, height=2,state='disabled')
+        back_botton.pack( side=BOTTOM  )
+
+        enter_botton=Button(bottom_left_frame,text="Enter |>",fg="Black", width=7, height=2,state='disabled')
+        enter_botton.pack( side=BOTTOM  )
+
+        self.decrypt_botton=Button(bottom_left_frame,text="Decrypt",fg="Black", width=7, height=2, command=self.encrypt,state='disabled')
+        self.decrypt_botton.pack( side=BOTTOM  )
+
+        encrypt_botton=Button(bottom_left_frame,text="Encrypt",fg="Black", width=7, height=2,command=self.decrypt,state='disabled')
+        encrypt_botton.pack( side=BOTTOM )
+
+        load_tree_botton=Button(bottom_left_frame,text="Load Tree",fg="black", width=7, height=2,command= self.load_tree)
+        load_tree_botton.pack()
+
+
+        root.mainloop()
+
+    def get_combobox_value(self):
+        return self.ips.get()
 
     def encrypt(self):
-        self.server.encrypt(self.ip.get(), self.file.get())
+        #print "hi"
+        #self.decrypt_botton.config(state='disabled')
+        self.server.encrypt(self.get_combobox_value(), self.get_path())
 
     def decrypt(self):
-        self.server.decrypt(self.ip.get(), self.file.get())
+        self.server.decrypt(self.get_combobox_value(), self.get_path())
 
 
-g = GUI()
+
+    def get_path(self):
+
+        path = os.path.join(*self.path)
+        return os.path.join(path,self.get_selected_item())
+
+    def update_scrollbar(self, lst):
+        for val in lst:
+            if type(val) == tuple:
+                self.tree_list.insert(END, val[0])
+                self.folders.append(val[0])
+            else:
+                self.tree_list.insert(END, val)
+                self.folders.append(val)
+
+    def update_ips(self):
+        self.ip_list = self.server.get_ip_list()
+        # self.ips.config(values= self.ip_list)
+        self.ips['values'] = self.ip_list
+
+    def get_selected_item(self):
+        return self.tree_list.get(self.tree_list.curselection())
+
+    def load_tree(self):
+        ip = self.get_combobox_value()
+        soc =  self.server.socket_by_ip(ip)
+        tree = self.server.client_dict[soc][1]
+        self.update_scrollbar(tree)
+
+
+
+
+g=GUI()
